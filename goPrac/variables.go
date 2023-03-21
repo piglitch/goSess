1e24d7630a1fbd92e5f734423109bd3a8aad3c0f
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-21 21:56:11 +0530

    float and int

--- goPrac/variables.go
+++ goPrac/variables.go
@@ -7,13 +7,16 @@ var Age int = 33
 func main()  {					// main function is executed by default when we run the main package.
 	fmt.Println(Age)			// Can show the value of Age despite the Shadowing(see Note1) if we print its value before assigning the new value.
 	var Name string = "Avi"     // Standard way of decclaring the variables.
-	var Age int32 = 22
+	var Age float32 = 22.5
+	var L int
+	L = int(Age)
 
 	j:= "a string"				// Assigns default value to the variables. 
 	k:= 44
 
 	fmt.Println(Name)			// Prints a new line after output.
-	fmt.Print(Age)				// Prints output in the default format.
+	fmt.Print(Age, " | ")				// Prints output in the default format.
+	fmt.Println(L)
 	println() // To print the next output in a new line.
 	fmt.Printf("%v, %T", j, k)	// Used to see the value(%v) and the type(%T) of the stored value.
 	println()

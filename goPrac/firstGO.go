2a488484190776d37f3669b39fef5494b6696d87
Avi Banerjee <avibanerjee2000@gmail.com>
2023-03-21 19:36:13 +0530

    Add files via upload

---
+++ goPrac/firstGO.go
@@ -0,0 +1,10 @@
+package main
+import ("fmt")
+
+func main()  {
+	var Name string = "Avi"
+	var Age int32 = 2205
+
+	fmt.Println(Name)
+	fmt.Print(Age)
+}
---
+++ goPrac/operations.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main(){
+	var num int
+	var num1 int
+	num = 7
+	num1 = 8
+	fmt.Println(num*num1)
+	
+}

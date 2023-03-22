7d8a4eb9f5b96401819ff78cfcdf658dcc2e9aa8
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-23 01:59:03 +0530

    scan

--- goPrac/operations.go
+++ goPrac/operations.go
@@ -3,10 +3,15 @@ package main
 import ("fmt")
 
 func main(){
-	var num int
-	var num1 int
-	num = 7
-	num1 = 8
-	fmt.Println(num*num1)
+	var num, num1 int
+
+	fmt.Print("Type first number: ")	
+	fmt.Scan(&num) // To take input 
+	fmt.Print("Type second number: ")
+	fmt.Scan(&num1)
+
+	var mult  int = num*num1
+
+	fmt.Println("Value after multiplication: ", mult)
 	
 }

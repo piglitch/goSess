2dbfcf5cd151f98e222183b0f8fc65a1e83e3b3a
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-23 19:19:01 +0530

    ops2

--- goPrac/operations.go
+++ goPrac/operations.go
@@ -4,14 +4,34 @@ import ("fmt")
 
 func main(){
 	var num, num1 int
+	var ops string
 
 	fmt.Print("Type first number: ")	
 	fmt.Scan(&num) // To take input 
 	fmt.Print("Type second number: ")
 	fmt.Scan(&num1)
+	fmt.Println("1. Sum\n2. Sub\n3. Mult\n4. Div\n5. Remainder")
+	fmt.Print("Choose one from 1, 2, 3, 4, 5: ")
+	fmt.Scan(&ops)
 
-	var mult  int = num*num1
 
-	fmt.Println("Value after multiplication: ", mult)
+	switch ops{
+	case "1":
+		fmt.Println("Value after multiplication: ", num+num1)
+	case "2":
+		fmt.Println("Value after multiplication: ", num-num1)
+	case "3":
+		fmt.Println("Value after multiplication: ", num*num1)
+	case "4":
+		fmt.Println("Value after multiplication: ", num/num1)
+	case "5":
+		fmt.Println("Value after multiplication: ", num%num1)			
+	
+	default:
+		fmt.Println("Wrong input! Please choose from 1, 2, 3, 4, 5")	
+	}
+	
+
+	
 	
 }

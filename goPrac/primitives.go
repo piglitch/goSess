f97bad2add70dab6f93a39350cdc1947f84a3063
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-23 20:00:36 +0530

    bitshift

--- goPrac/primitives.go
+++ goPrac/primitives.go
@@ -5,6 +5,14 @@ import (
 func main(){
 	n:= 1 == 1
 	m:= 1 == 2
-	fmt.Printf("%v, %T", n, n)
-	fmt.Printf("%v, %T", m, m)
+	fmt.Printf("%v, %T\n", n, n) //Prints true because 1 == 1
+	fmt.Printf("%v, %T\n", m, m)	//Prints false bacause 1 is not equal to 2
+	bitshift()
+} 
+
+func bitshift(){
+	a:=8
+	fmt.Println(a<<3)
+	fmt.Println(a>>3)
 }
+

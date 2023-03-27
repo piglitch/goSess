671e21eb27b3684a6fe781ab5d568a4019157ed0
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-27 20:18:15 +0530

    calling global

--- goPrac/constants.go
+++ goPrac/constants.go
@@ -1,7 +1,13 @@
 package main
 import ("fmt")
+const myConst int = 24	// Declared globally
 
 func main(){
-	const myConst int = 42
+	const myConst int = 42 
+	
+	fmt.Printf("%v, %T\n", myConst, myConst) // Returns the value declared inside the function
+	anotherConst() // Call a function that returns the value of that was globally declared
+}
+func anotherConst(){
 	fmt.Printf("%v, %T\n", myConst, myConst)
 }

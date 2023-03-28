2c667f3f0bf1e72ae976b7a636d6298f809fd021
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-28 17:05:22 +0530

    slice

--- goPrac/arrays.go
+++ goPrac/arrays.go
@@ -6,10 +6,22 @@ func main(){
 	students[2] = "Antony"
 	fmt.Printf("Students: %v\n", students)
 	arrayOfArrays()
+	slice()
 }
 
 func arrayOfArrays(){
 	var randomMatrix [2][2]int = [2][2]int{[2]int{1, 0}, [2]int{0,1}}
 	fmt.Println(randomMatrix)
+	fmt.Println(len(randomMatrix))
+	fmt.Println(cap(randomMatrix))
+}
+
+func slice(){
+	a := []int{1,2,3,4,5,6,7,8,9}
+	b := a[:] //slice of all
+	c := a[5:] // from the 6th element to the end
+	d := a[:8] // upto 8th element
+	e := a[4:7] //5th to 7h element
+	fmt.Println(a,b,c,d,e) 
 }
 

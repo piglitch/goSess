26be1071b28bd7d443f27c1eeb4f338a71704ec2
piglitch <73684837+piglitch@users.noreply.github.com>
2023-03-31 23:57:53 +0530

    if

--- goPrac/ifElse.go
+++ goPrac/ifElse.go
@@ -4,5 +4,18 @@ import (
 )
 
 func main(){
-	
+	var guess int
+	number:= 50
+	fmt.Print("Take a guess: ")
+	fmt.Scan(&guess)
+
+	if number > guess  {
+		fmt.Println("Too low")		
+	}
+	if number < guess {
+		fmt.Println("Too high")
+	}
+	else {
+		
+	}
 }

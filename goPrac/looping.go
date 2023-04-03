a73e661c5283f767d03d8a8fe2b3915c39da161e
piglitch <73684837+piglitch@users.noreply.github.com>
2023-04-03 19:57:36 +0530

    mimic do while

--- goPrac/looping.go
+++ goPrac/looping.go
@@ -8,4 +8,19 @@ func main(){
 	for i:=0; i<5; i++{
 		fmt.Println("name's bond")
 	}
+	doWh()
+}
+
+// Using if statement to mimic do while loop
+
+func doWh(){
+	i:=0
+	for{
+		fmt.Println("Name's bond")
+		i++
+		if i == 7 {
+			break			
+		}
+	}
+	
 }

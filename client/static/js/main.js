5a134304ec00a9f4b240f33935d7956013b442a0
Avi <avibanerjee2000@gmail.com>
2024-09-01 00:19:15 +0530

    used reusable functions

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -6,6 +6,10 @@ const newButton = document.createElement('button');
 newButton.innerText = "Click to change the background"
 
 newButton.addEventListener("click", () => {
+  if (Body.style.backgroundColor === 'teal') {
+    Body.style.backgroundColor = 'beige';
+    return;
+  }
   Body.style.backgroundColor = 'teal' 
 })
 
--- server/server.go
+++ server/server.go
@@ -3,30 +3,39 @@ package main
 import (
     "fmt"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
     // Serve the index.html file at the root
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("Serving index.html")
-        http.ServeFile(w, r, "../client/index.html")
-    })
+    r := mux.NewRouter()
 
+    r.HandleFunc("/", showHomePage)
     // Serve other HTML files based on the route
-    http.HandleFunc("/messages", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("Serving messages.html")
-        http.ServeFile(w, r, "../client/messages.html")
-    })
+    r.HandleFunc("/messages", showMessages)
 
     // Serve all other static files (JS, CSS, images, etc.)
-    // fs := http.FileServer(http.Dir("../client/static/js/main.js"))
-    // http.Handle("../client/static/js/", http.StripPrefix("../client/static/js/", fs))
-    http.HandleFunc("/client/static/js/main.js", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("Serving messages.html")
-        http.ServeFile(w, r, "../client/static/js/main.js")
-    })
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    // http.HandleFunc("/client/static/js/main.js", func(w http.ResponseWriter, r *http.Request) {
+    //     fmt.Println("Serving messages.html")
+    //     http.ServeFile(w, r, "../client/static/js/main.js")
+    // })
     //fmt.Println(fs)
+
+    http.Handle("/", r)
     fmt.Println("Starting server on :8080")
     http.ListenAndServe(":8080", nil)
+}
+
+func showHomePage(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Serving index.html")
+    http.ServeFile(w, r, "../client/index.html")
+}
+
+func showMessages(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Serving messages.html")
+    http.ServeFile(w, r, "../client/messages.html")
 }
 
